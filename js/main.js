5f8a0516221acd4b3742d2fda0c9b355dca5ed80
VovchikMusulman <slavaatam83@gmail.com>
2025-02-03 11:02:56 +0700

    Vue.js-5

--- js/main.js
+++ js/main.js
@@ -10,9 +10,22 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        inventory: 100,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
+
 
 
 
