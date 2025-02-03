23df0b3ac4b54de615ca63f3fd98b1f045b532d7
VovchikMusulman <slavaatam83@gmail.com>
2025-02-03 10:29:13 +0700

    Vue.js-2

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        altText: "A pair of socks"
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100
     }
 })
 
