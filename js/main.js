654bda1fb3a850e36eb16433e4a59bae89f26b0d
VovchikMusulman <slavaatam83@gmail.com>
2025-02-04 10:11:37 +0700

    Vue.js-7

--- js/main.js
+++ js/main.js
@@ -4,31 +4,31 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks.",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         onSale: true,
-        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         inventory: 100,
+        selectedVariant: 0,
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
     },
     methods: {
         addToCart() {
@@ -38,8 +38,26 @@ let app = new Vue({
             if (this.cart > 0) {
                 this.cart -= 1
             }
-        }
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        sale() {
+            return this.onSale ? `${this.brand} ${this.product} on sale!` : `${this.brand} ${this.product} on not sale.`;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    }
 })
 
 
